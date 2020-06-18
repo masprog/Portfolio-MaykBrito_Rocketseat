d2bf3c441582a5b2f0fae72c958ed51adfff8b88
masprog <maurolove09@gmail.com>
2020-06-18 07:57:21 -0300

    update page about - passing date from back to front

--- server.js
+++ server.js
@@ -14,13 +14,35 @@ server.set("view engine","njk")
 
 // configurar o nunjucks 
 nunjucks.configure("views", {
-  express:server
+  express:server,
+  autoescape: false // funcionalidade para não trazer dados em formato HTML
 })
 
 // criar a rota da página principal
 
 server.get("/", function(req, res){
-  return res.render("about")
+
+  //enviar dados para a page about do  back para o front
+  const about = {
+      avatar_url: "https://pbs.twimg.com/profile_images/1190279367429148672/qbeF0cUT_400x400.jpg",
+      name: "Mayk Brito",
+      role: "Instrutor - Rocketseat",
+      description: `Programador full-stack, focado em trazer o melhor ensino para iniciantes em programação, colaborador da
+      <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>`,
+      links: [
+        {name:"Github", url: "https://github.com/maykbrito/"},
+        {name:"Twitter", url: "https://twitter.com/maykbrito/"},
+        {name:"Linkdin", url: "https://www.linkdin.com/in/maykbrito/"}
+      ]
+
+  }
+  // criar a rota para o about 
+  //return res.render("about", )
+
+  //criar a rota para o about e mandando dados para o front
+  return res.render("about", {about: about})
+  
+
 })
 
 // criar a rota para o portfolio 
